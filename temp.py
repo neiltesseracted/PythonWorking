e7de3664a4afa96d0680b9bf7205be3213fb171a
neiltesseracted <rexeneo@gmail.com>
2015-11-21 16:21:56 -0500

    blabla

---
+++ temp.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import os
+import csv
+import pandas as pd
+os.chdir('/Users/neo/PythonWorking/')
+
+"""
+with open('./lecz/lecz-urban-rural-population-land-area-estimates_continent-90m.csv','rU') as inputFile:
+    header=next(inputFile).rstrip().split(',')
+    for line in inputFile:
+        line=line.rstrip().split(',')
+        if line[0]=='Asia':
+            print(line)
+    print(header[0])
+"""
+        
+dataframe=pd.read_csv('lecz/lecz-urban-rural-population-land-area-estimates_codebook.csv')
+dataframe2=pd.read_csv('lecz/lecz-urban-rural-population-land-area-estimates_continent-90m.csv')
+
+
+        
+    
