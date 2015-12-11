46b7fbf0657db5039c604f31f7a160347a57173a
neiltesseracted <rexeneo@gmail.com>
2015-12-11 16:28:46 -0500

    rep

--- U3/rep_query.py
+++ U3/rep_query.py
@@ -6,23 +6,28 @@ import collections
 from dateutil.parser import parse
 
 r = requests.get('http://www.citibikenyc.com/stations/json', stream=True)
-
-#take the string and parse it into a Python datetime object
-exec_time = parse(r.json()['executionTime'])
-
 con = lite.connect('citi_bike.db')
 cur = con.cursor()
-
 with con:
-    cur.execute('INSERT INTO available_bikes (execution_time) VALUES (?)', (exec_time.strftime('%s'),))
+    cur.execute('DELETE FROM available_bikes')
 
-id_bikes = collections.defaultdict(int) #defaultdict to store available bikes by station
+for i in range(60):
+    #take the string and parse it into a Python datetime object
+    exec_time = parse(r.json()['executionTime'])
 
-#loop through the stations in the station list
-for station in r.json()['stationBeanList']:
-    id_bikes[station['id']] = station['availableBikes']
+    with con:
+        cur.execute('INSERT INTO available_bikes (execution_time) VALUES (?)', (exec_time.strftime('%s'),))
 
-with con:
-    for k, v in id_bikes.iteritems():
-        cur.execute("UPDATE available_bikes SET _" + str(k) + " = " + str(v) + " WHERE execution_time = " + exec_time.strftime('%s') + ";")
+    id_bikes = collections.defaultdict(int) #defaultdict to store available bikes by station
 
+    #loop through the stations in the station list
+    for station in r.json()['stationBeanList']:
+        id_bikes[station['id']] = station['availableBikes']
+
+    with con:
+        for k, v in id_bikes.iteritems():
+            cur.execute("UPDATE available_bikes SET _" + str(k) + " = " + str(v) + " WHERE execution_time = " + exec_time.strftime('%s') + ";")
+
+    time.sleep(60)
+
+con.close()
