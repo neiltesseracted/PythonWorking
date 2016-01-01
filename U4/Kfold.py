3061865c55243a39babbc654286d438b9fa4fd9e
neiltesseracted <rexeneo@gmail.com>
2016-01-01 03:14:02 -0500

    random forest

--- U4/Kfold.py
+++ U4/Kfold.py
@@ -8,15 +8,6 @@ df=pd.read_csv('./U2/loansData_clean.csv')
 rate = df['Interest.Rate']
 amt = df['Amount.Requested']
 fico = df['FICO.Score']
-# they're all series type. needs reshape
-# y=np.matrix(rate).transpose()
-# x1 = np.matrix(fico).transpose()
-# x2 = np.matrix(amt).transpose()
-#
-# x=np.column_stack([x1,x2])
-# X=sm.add_constant(x)
-# f=smf.ols(formula='rate ~ amt + fico', data=df).fit()
-# f.summary()
 
 kf = KFold(rate.__len__(), n_folds=10, shuffle=True)
 f=[]
@@ -32,9 +23,9 @@ for i, fold in enumerate(kf):
     dfv=df.iloc[fold[1]]
     yhat=m.params[0]+m.params[1]*dfv['Amount.Requested'] + m.params[2]*dfv['FICO.Score']
     y=dfv['Interest.Rate']
-    mae=abs(y-yhat).mean()
+    mae=(abs(y-yhat)/y).mean()  # normalize
     ssres=((y-yhat)**2).sum()
-    mse=((y-yhat)**2).mean()
+    mse=(((y-yhat)/y)**2).mean()    # normalize
     ybar=y.mean()
     sstot=((y-ybar)**2).sum()
     r2=1-ssres/sstot
---
+++ U4/Random_Forest.py
@@ -0,0 +1,53 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+import sklearn.metrics as skm
+import pylab as pl
+
+datafolder="../PythonWorking_largedata/UCI HAR Dataset/"
+
+trainx=pd.read_fwf(datafolder+"train/X_train.txt", header=None, widths=[16]*561)
+trainy=pd.read_csv(datafolder+"train/Y_train.txt", header=None)
+
+header=pd.read_csv(datafolder+"features.txt", header=None, sep=' ')[1]
+trainx.columns=header
+
+rfc = RandomForestClassifier(n_estimators=500, oob_score=True)
+print("fitting model...")
+model=rfc.fit(trainx, trainy.values.ravel())
+# if no .ravel(), warns this:
+# DataConversionWarning: A column-vector y was passed when a 1d array was expected.
+# Please change the shape of y to (n_samples,), for example using ravel().
+print("rfc done." + " oob_score_ is " + str(rfc.oob_score_))
+
+############## rfc done. ###############
+
+fi = rfc.feature_importances_
+# get top 10 features' index
+t10idx=fi.argsort()[-10::]
+# get top 10 features' names
+t10 = trainx.columns[t10idx]
+
+# test set
+testx=pd.read_fwf(datafolder+"test/X_test.txt", header=None, widths=[16]*561)
+testy=pd.read_csv(datafolder+"test/y_test.txt", header=None)
+testx.columns=header
+
+test_pred=rfc.predict(testx)
+print("test data rfc score= %f" % (rfc.score(testx, testy)) )
+
+# what's confusion matrix?
+test_cm = skm.confusion_matrix(testy, test_pred)
+
+pl.matshow(test_cm)
+pl.title('Confusion matrix for test data')
+pl.colorbar()
+pl.show()
+
+# Accuracy
+print("Accuracy = %f" %(skm.accuracy_score(testy,test_pred)))
+# Precision
+print("Precision = %f" %(skm.precision_score(testy,test_pred)))
+# Recall
+print("Recall = %f" %(skm.recall_score(testy,test_pred)))
+# F1 Score
+print("F1 score = %f" %(skm.f1_score(testy,test_pred)))
