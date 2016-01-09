edc4cf7a8b5a5b71fed4b5c2b69b6ff57abbafcb
neiltesseracted <rexeneo@gmail.com>
2016-01-09 01:49:07 -0500

    kmeans

---
+++ U4/kMeans.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.naive_bayes import GaussianNB
+from scipy.cluster.vq import kmeans, vq, whiten
+
+df=pd.read_csv('./U4/un.csv')
+
+# only need 4 col
+df=df[['lifeMale','lifeFemale','infantMortality','GDPperCapita']]
+# get rid of NaNs
+df=df[df['lifeMale'].notnull()][df['lifeFemale'].notnull()][df['infantMortality'].notnull()][df['GDPperCapita'].notnull()]
+
+whitened=whiten(df)
+distavg=[]
+for k in range(1,11):
+    centroids, _ =kmeans(whitened[:,3],k)
+    _, idxdist = vq(whitened[:,3],centroids)
+    distavg.append(idxdist.mean())
+# plot average distortions with different k
+plt.plot(distavg, '-ob', markersize=3)
+
+# k = 3
+plt.figure()
+centroids, dist =kmeans(whitened,3)
+idx, idxdist = vq(whitened,centroids)
+plt.plot(df.iloc[idx==0, 3], df.iloc[idx==0, 2], 'ob',
+            df.iloc[idx==1, 3], df.iloc[idx==1, 2], 'or',
+            df.iloc[idx==2, 3], df.iloc[idx==2, 2], 'og')
+
+# GDPperCapita as base for kmeans
+plt.figure()
+centroids, dist =kmeans(whitened[:,3],3)
+idx, idxdist = vq(whitened[:,3],centroids)
+plt.plot(df.iloc[idx==0, 3], df.iloc[idx==0, 2], 'ob',
+            df.iloc[idx==1, 3], df.iloc[idx==1, 2], 'or',
+            df.iloc[idx==2, 3], df.iloc[idx==2, 2], 'og')
+
+
