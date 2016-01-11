64b19131b1fac81caf08ddad29aeed787a10b5bc
neiltesseracted <rexeneo@gmail.com>
2016-01-11 14:33:08 -0500

    kmeans revise

--- U4/kMeans.py
+++ U4/kMeans.py
@@ -22,11 +22,14 @@ plt.plot(distavg, '-ob', markersize=3)
 
 # k = 3
 plt.figure()
-centroids, dist =kmeans(whitened,3)
-idx, idxdist = vq(whitened,centroids)
+centroids, dist =kmeans(whitened[:,[2,3]],3)
+idx, idxdist = vq(whitened[:,[2,3]],centroids)
 plt.plot(df.iloc[idx==0, 3], df.iloc[idx==0, 2], 'ob',
             df.iloc[idx==1, 3], df.iloc[idx==1, 2], 'or',
             df.iloc[idx==2, 3], df.iloc[idx==2, 2], 'og')
+plt.xlabel(df.columns[3])
+plt.ylabel(df.columns[2])
+plt.savefig('kmeans_mortality_v_gdp_2dcentroids')
 
 # GDPperCapita as base for kmeans
 plt.figure()
@@ -35,5 +38,8 @@ idx, idxdist = vq(whitened[:,3],centroids)
 plt.plot(df.iloc[idx==0, 3], df.iloc[idx==0, 2], 'ob',
             df.iloc[idx==1, 3], df.iloc[idx==1, 2], 'or',
             df.iloc[idx==2, 3], df.iloc[idx==2, 2], 'og')
+plt.xlabel(df.columns[3])
+plt.ylabel(df.columns[2])
+plt.savefig('kmeans_mortality_v_gdp_1dcentroids')
 
 
